8ca4e21fb4aa5eb20a433e8e69aa6d4a3f5700e4
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 16:36:34 +0530

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -10,7 +10,7 @@ def __list_all_modules():
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
                    and f.endswith(".py")
-                   and not f.endswith('__init__.py') 
+                   and not f.endswith('__init__.py')]
     return all_modules
 def get_all_plugins() -> List[str]:
     """ list all plugins """
