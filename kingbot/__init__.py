8c5b653dca91efc0e0efb119974b0550781c281b
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 16:21:10 +0530

    Update __init__.py

--- kingbot/__init__.py
+++ kingbot/__init__.py
@@ -85,4 +85,3 @@ async def edrep(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
     await func(**{k: v for k, v in kwargs.items() if k in spec})
-BotUN= setbot.get_me().username
