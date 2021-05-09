8639df6c4de0f52af0012ceb0ed22ac71b900f80
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 17:37:10 +0530

    Update starter.py

--- assistant/starter.py
+++ assistant/starter.py
@@ -1,9 +1,11 @@
 from pyrogram import client , filters
 from pyrogram.types import Message
-from kingbot import kingbot , AdminSettings, HNDLR
+from kingbot import kingbot 
 __MODULE__ = "start"
 __HELP__ = """**Just a start message**
 """
+HNDLR="."
+AdminSettings= [1359459092]
 @kingbot.on_message(filters.command("start",HNDLR) & filters.user(AdminSettings))
 async def start(_, message: Message):
     await message.reply_text(
