8ebeefa72644fe2ef0ec42220f43f4eec522814d
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 17:13:10 +0530

    Update __main__.py

--- kingbot/__main__.py
+++ kingbot/__main__.py
@@ -23,6 +23,178 @@ HNDLR="."
 async def get_runtime():
     return BOT_RUNTIME
 
+
+async def reload_userbot():
+    await kingbot.start()
+    for modul in ALL_MODULES:
+        imported_module = importlib.import_module("utilss." + modul)
+        imported_module = importlib.import_module("ownerr." + modul)
+        imported_module = importlib.import_module("adminss." + modul)
+        importlib.reload(imported_module)
+
+
+async def reinitial_restart():
+    await get_bot()
+    await get_self()
+
+
+async def reboot():
+    global BOT_RUNTIME, HELP_COMMANDS
+    importlib.reload(importlib.import_module("adminss"))
+    importlib.reload(importlib.import_module("ownerr"))
+    importlib.reload(importlib.import_module("utilss"))
+    importlib.reload(importlib.import_module("assistant"))
+    # await setbot.send_message(Owner, "Bot is restarting...")
+    await setbot.restart()
+    await kingbot.restart()
+    await reinitial_restart()
+    # Reset global var
+    BOT_RUNTIME = 0
+    HELP_COMMANDU = {}
+    HELP_COMMANDA = {}
+    HELP_COMMANDO = {}
+    HELP_COMMANDAST = {}
+    # Assistant bot
+    
+    for modul in ALL_UTILS:
+        imported_module = importlib.import_module("utilss." + modul)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDU:
+                HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
+        importlib.reload(imported_module)
+    for modula in ALL_ADMINN:
+        imported_module = importlib.import_module("adminss." + modula)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDA:
+                HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
+        importlib.reload(imported_module)
+    for modulst in ALL_OWN:
+        imported_module = importlib.import_module("ownerr." + modulst)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDO:
+                HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
+        importlib.reload(imported_module)
+    for modula in ALL_AST:
+            imported_module = importlib.import_module("assistant." + modul)
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                imported_module.__MODULE__ = imported_module.__MODULE__
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                if not imported_module.__MODULE__.lower() in HELP_COMMANDAST:
+                    HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
+                else:
+                    raise Exception("Can't have two modules with the same name! Please change one")
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
+            importlib.reload(imported_module)
+    HELP_COMMANDS = {**HELP_COMMANDA,**HELP_COMMANDU,**HELP_COMMANDO, **HELP_COMMANDAST}
+
+# await setbot.send_message(Owner, "Restart successfully!")
+
+async def restart_all():
+    # Restarting and load all plugins
+    asyncio.get_event_loop().create_task(reboot())
+
+
+RANDOM_STICKERS = ["CAADAgAD6EoAAuCjggf4LTFlHEcvNAI", "CAADAgADf1AAAuCjggfqE-GQnopqyAI",
+                   "CAADAgADaV0AAuCjggfi51NV8GUiRwI"]
+
+
+async def except_hook(errtype, value, tback):
+    sys.__excepthook__(errtype, value, tback)
+    errors = traceback.format_exception(etype=errtype, value=value, tb=tback)
+    button = InlineKeyboardMarkup([[InlineKeyboardButton("🐞 Report bugs", callback_data="report_errors")]])
+    text = "An error has accured!\n\n```{}```\n".format("".join(errors))
+    if errtype == ModuleNotFoundError:
+        text += "\nHint: Try this in your terminal `pip install -r requirements.txt`"
+    await setbot.send_message(Owner, text, reply_markup=button)
+
+
+async def reinitial():
+    await kingbot.start()
+    await setbot.start()
+    await get_self()
+    await get_bot()
+    await kingbot.stop()
+    await setbot.stop()
+
+
+async def start_bot():
+    # sys.excepthook = except_hook
+    print("----- Checking user and bot... -----")
+    await reinitial()
+    print("----------- Check done! ------------")
+    # Assistant bot
+    await setbot.start()
+    await kingbot.start()
+    for modul in ALL_UTILS:
+        imported_module = importlib.import_module("utilss." + modul)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDU:
+                HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
+    for modula in ALL_ADMINN:
+        imported_module = importlib.import_module("adminss." + modula)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDA:
+                HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
+    for modulst in ALL_OWN:
+        imported_module = importlib.import_module("ownerr." + modulst)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDO:
+                HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Can't have two modules with the same name! Please change one")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
+    for modula in ALL_AST:
+            imported_module = importlib.import_module("assistant." + modul)
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                imported_module.__MODULE__ = imported_module.__MODULE__
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+                if not imported_module.__MODULE__.lower() in HELP_COMMANDAST:
+                    HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
+                else:
+                    raise Exception("Can't have two modules with the same name! Please change one")
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
+            await idle()
+    HELP_COMMANDS = {**HELP_COMMANDA,**HELP_COMMANDU,**HELP_COMMANDO, **HELP_COMMANDAST}
+AdminSettings= get_self().AdminSettings
+Owner= get_self().Owner
+if __name__ == '__main__':
+    BOT_RUNTIME = int(time.time())
+    loop.run_until_complete(start_bot())
 @kingbot.on_message(filters.command("help",HNDLR) & filters.user(AdminSettings))
 async def h_lp(_ , message):
   BotUN= setbot.get_me().username
@@ -193,14 +365,6 @@ async def cbire(_ , cbq: CallbackQuery):
    if cdt == "kloz":
          cbq.message.delete()
 
-async def reload_userbot():
-    await kingbot.start()
-    for modul in ALL_MODULES:
-        imported_module = importlib.import_module("utilss." + modul)
-        imported_module = importlib.import_module("ownerr." + modul)
-        imported_module = importlib.import_module("adminss." + modul)
-        importlib.reload(imported_module)
-
 async def help_button_callback(_, __, query):
     if re.match(r"help_", query.data):
         return True
@@ -242,166 +406,3 @@ async def help_button(_, query):
             ),
         )
     await query.answer()
-async def reinitial_restart():
-    await get_bot()
-    await get_self()
-
-
-async def reboot():
-    global BOT_RUNTIME, HELP_COMMANDS
-    importlib.reload(importlib.import_module("adminss"))
-    importlib.reload(importlib.import_module("ownerr"))
-    importlib.reload(importlib.import_module("utilss"))
-    importlib.reload(importlib.import_module("assistant"))
-    # await setbot.send_message(Owner, "Bot is restarting...")
-    await setbot.restart()
-    await kingbot.restart()
-    await reinitial_restart()
-    # Reset global var
-    BOT_RUNTIME = 0
-    HELP_COMMANDU = {}
-    HELP_COMMANDA = {}
-    HELP_COMMANDO = {}
-    HELP_COMMANDAST = {}
-    # Assistant bot
-    
-    for modul in ALL_UTILS:
-        imported_module = importlib.import_module("utilss." + modul)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDU:
-                HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
-        importlib.reload(imported_module)
-    for modula in ALL_ADMINN:
-        imported_module = importlib.import_module("adminss." + modula)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDA:
-                HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
-        importlib.reload(imported_module)
-    for modulst in ALL_OWN:
-        imported_module = importlib.import_module("ownerr." + modulst)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDO:
-                HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
-        importlib.reload(imported_module)
-    for modula in ALL_AST:
-            imported_module = importlib.import_module("assistant." + modul)
-            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-                imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-                if not imported_module.__MODULE__.lower() in HELP_COMMANDAST:
-                    HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
-                else:
-                    raise Exception("Can't have two modules with the same name! Please change one")
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
-            importlib.reload(imported_module)
-    HELP_COMMANDS = {**HELP_COMMANDA,**HELP_COMMANDU,**HELP_COMMANDO, **HELP_COMMANDAST}
-
-# await setbot.send_message(Owner, "Restart successfully!")
-
-async def restart_all():
-    # Restarting and load all plugins
-    asyncio.get_event_loop().create_task(reboot())
-
-
-RANDOM_STICKERS = ["CAADAgAD6EoAAuCjggf4LTFlHEcvNAI", "CAADAgADf1AAAuCjggfqE-GQnopqyAI",
-                   "CAADAgADaV0AAuCjggfi51NV8GUiRwI"]
-
-
-async def except_hook(errtype, value, tback):
-    sys.__excepthook__(errtype, value, tback)
-    errors = traceback.format_exception(etype=errtype, value=value, tb=tback)
-    button = InlineKeyboardMarkup([[InlineKeyboardButton("🐞 Report bugs", callback_data="report_errors")]])
-    text = "An error has accured!\n\n```{}```\n".format("".join(errors))
-    if errtype == ModuleNotFoundError:
-        text += "\nHint: Try this in your terminal `pip install -r requirements.txt`"
-    await setbot.send_message(Owner, text, reply_markup=button)
-
-
-async def reinitial():
-    await kingbot.start()
-    await setbot.start()
-    await get_self()
-    await get_bot()
-    await kingbot.stop()
-    await setbot.stop()
-
-
-async def start_bot():
-    # sys.excepthook = except_hook
-    print("----- Checking user and bot... -----")
-    await reinitial()
-    print("----------- Check done! ------------")
-    # Assistant bot
-    await setbot.start()
-    await kingbot.start()
-    AdminSettings= get_self().AdminSettings
-    for modul in ALL_UTILS:
-        imported_module = importlib.import_module("utilss." + modul)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDU:
-                HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDU[imported_module.__MODULE__.lower()] = imported_module
-    for modula in ALL_ADMINN:
-        imported_module = importlib.import_module("adminss." + modula)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDA:
-                HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDA[imported_module.__MODULE__.lower()] = imported_module
-    for modulst in ALL_OWN:
-        imported_module = importlib.import_module("ownerr." + modulst)
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if not imported_module.__MODULE__.lower() in HELP_COMMANDO:
-                HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
-            else:
-                raise Exception("Can't have two modules with the same name! Please change one")
-        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-            HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module
-    for modula in ALL_AST:
-            imported_module = importlib.import_module("assistant." + modul)
-            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-                imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-                if not imported_module.__MODULE__.lower() in HELP_COMMANDAST:
-                    HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
-                else:
-                    raise Exception("Can't have two modules with the same name! Please change one")
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELP_COMMANDAST[imported_module.__MODULE__.lower()] = imported_module
-            await idle()
-    HELP_COMMANDS = {**HELP_COMMANDA,**HELP_COMMANDU,**HELP_COMMANDO, **HELP_COMMANDAST}
-
-Owner= get_self().Owner
-if __name__ == '__main__':
-    BOT_RUNTIME = int(time.time())
-    loop.run_until_complete(start_bot())
