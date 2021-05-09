3cb7d0b9ca728259415d76069be54c838909368a
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 16:05:34 +0530

    Update __init__.py

--- kingbot/__init__.py
+++ kingbot/__init__.py
@@ -85,7 +85,7 @@ async def get_bot():
     BotID = getbot.id
     BotName = getbot.first_name
     BotUsername = getbot.username
-BotUN= BotUsername
+BotUN= get_bot().BotUsername
 
 BASE = declarative_base()
 SESSION = mulaisql()
