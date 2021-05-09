d1dbcc2b48120c4b7ea5371b2171be945ea4c5fb
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 16:55:47 +0530

    Update starter.py

--- assistant/starter.py
+++ assistant/starter.py
@@ -4,7 +4,7 @@ from kingbot import kingbot , AdminSettings, HNDLR
 __MODULE__ = "start"
 __HELP__ = """**Just a start message**
 """
-@kingbot.on_message(filters.command(commands:"start",prefixes:HNDLR) & filters.user(AdminSettings))
+@kingbot.on_message(filters.command("start",HNDLR) & filters.user(AdminSettings))
 async def start(_, message: Message):
     await message.reply_text(
         "Shit u are allowed dear!! \n Only Kings wield this power" )
