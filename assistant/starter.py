b0767fa1a687b17ffcf054d7454adbd81ecce140
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-09 18:10:49 +0530

    Update starter.py

--- assistant/starter.py
+++ assistant/starter.py
@@ -1,12 +1,12 @@
 from pyrogram import client , filters
 from pyrogram.types import Message
-from kingbot import kingbot 
+from kingbot import kingbot , setbot
 __MODULE__ = "start"
 __HELP__ = """**Just a start message**
 """
-HNDLR="."
+HNDLR="/"
 AdminSettings= [1359459092]
-@kingbot.on_message(filters.command("start",HNDLR) & filters.user(AdminSettings))
+@setbot.on_message(filters.command("start",HNDLR) & filters.user(AdminSettings))
 async def start(_, message: Message):
     await message.reply_text(
         "Shit u are allowed dear!! \n Only Kings wield this power" )
