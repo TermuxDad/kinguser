8f11ba9eeca730565390d274865fa5393cb50966
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-13 10:36:59 +0530

    Update __main__.py

--- kingbot/__main__.py
+++ kingbot/__main__.py
@@ -207,7 +207,11 @@ async def start_bot():
                 raise Exception("Can't have two modules with the same name! Please change one")
         if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
             HELP_COMMANDO[imported_module.__MODULE__.lower()] = imported_module.__HELP__
-    await ser.getall(HELP_COMMANDS, HELP_COMMANDU, HELP_COMMANDA, HELP_COMMANDO, HELP_COMMANDAST)
+    ser.HC=HELP_COMMANDS
+    ser.HU=HELP_COMMANDU
+    ser.HA=HELP_COMMANDA
+    ser.HO=HELP_COMMANDO
+    ser.HAT=HELP_COMMANDAST
     HELP_COMMANDS = {**HELP_COMMANDA,**HELP_COMMANDU,**HELP_COMMANDO, **HELP_COMMANDAST}
     await idle()
 if __name__ == '__main__':
