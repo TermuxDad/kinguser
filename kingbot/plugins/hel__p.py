f49afd670f0cba447edcb10de4301cc50b721328
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-13 21:59:17 +0530

    Update hel__p.py

--- kingbot/plugins/hel__p.py
+++ kingbot/plugins/hel__p.py
@@ -161,9 +161,9 @@ async def cbire(_ , cbq: CallbackQuery):
 
 
 # help_button_create = filters.create(help_button_callback)
-@setbot.on_callback_query(filters.user(Adminsettings))
+@setbot.on_callback_query(filters.user(1359459092))
 async def help_button(_, query):  
-    await kingbot.send_message("me", "got query  
+    await kingbot.send_message("me", "got query") 
     HELP_COMMANDS = ser.HC
     mod_match = re.match(r"help_module\((.+?)\)", query.data)
     back_match = re.match(r"help_back", query.data)
