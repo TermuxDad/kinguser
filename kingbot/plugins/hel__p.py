eb8fbcc134acbdff5a21699d4bc0742d985f4874
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-13 19:02:50 +0530

    Update hel__p.py

--- kingbot/plugins/hel__p.py
+++ kingbot/plugins/hel__p.py
@@ -2,13 +2,13 @@ from uti.misc import paginate_modules
 from pyrogram import idle
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton , InlineQuery ,Message, CallbackQuery, InlineQueryResultPhoto
 from pyrogram import filters 
-from kingbot import kingbot ,setbot , vr, Adminsettings
+from kingbot import kingbot ,setbot , vr,Admins
 from uti.serra import ser
 import re
 
 
 
-@kingbot.on_message(filters.command("help",vr.get("HNDLR")) & filters.user(AdminSettings))
+@kingbot.on_message(filters.command("help",vr.get("HNDLR")) & filters.user(Admins))
 async def h_lp(_ , message):
   res=await kingbot.get_inline_bot_results("Devilkalund2bot", "hlpin")
   await kingbot.send_inline_bot_result(message.chat.id, res.query_id, res.results[0].id)
