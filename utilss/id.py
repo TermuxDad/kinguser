40b83f761a14839dfabe42b0efb6dda435bbd1dd
Edguru <72596185+edguru@users.noreply.github.com>
2021-05-14 16:27:16 +0530

    Update id.py

--- utilss/id.py
+++ utilss/id.py
@@ -16,4 +16,4 @@ async def id(_, message):
         await message.reply(f"This chat's ID is: {message.chat.id}")
     else:
         test = f"This scumbag's ID is: {message.reply_to_message.from_user.id}\n\nThis chat's ID is: {message.chat.id}"
-        await message.reply(test)
+        await message.edit_text(test)
